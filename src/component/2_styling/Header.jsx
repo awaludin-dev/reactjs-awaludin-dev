a32260fcbc9a10e7871738c9119a77408fafe5d4
awaludin-dev <ubedsf@gmail.com>
2021-11-25 04:13:47 +0800

    create biodata

--- src/component/2_styling/Header.jsx
+++ src/component/2_styling/Header.jsx
@@ -7,12 +7,12 @@ export default class Header extends React.Component {
         return (
             <Navbar bg="info" expand="lg" fixed="top">
                 <Container>
-                    <Navbar.Brand href="#home">Awaludin</Navbar.Brand>
+                    <Navbar.Brand href="#">Awaludin</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="#biodata">Biodata</Nav.Link>
+                        <Nav.Link href="#skill">Skill</Nav.Link>
                         <Nav.Link href="#contact">Contact</Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
--- src/component/2_styling/Main.jsx
+++ src/component/2_styling/Main.jsx
@@ -10,6 +10,37 @@ export default class Main extends React.Component {
                     <h2>Awaludin Nasir Ubadah</h2>
                     <div><button>Github Profile</button></div>
                 </div>
+                <div className="biodata" id="biodata">
+                    <h1>Biodata</h1>
+                    <p>Namaku adalah Awaludin Nasir Ubadah dan aku adalah anak ke 2 dari 4 bersaudara. Saat ini aku sedang berkuliah di Universitas Udayana jurusan Informatika. Aku bertemu banyak teman disini dan kagum akan budaya Bali yang sangat indah. Selain itu, perubahan kebiasaan dari SMA ke kuliah membuatku harus beradaptasi.</p>
+                    <p>Aku bukanlah anak yang jenius, hanyalah anak biasa. Kebetulan saja aku suka coding jadi aku ingin memperdalam, meskipun koding membuat kepalaku pusing. Error dan bug selalu menghantui setiap programku, tapi hasil akhir yang memuaskan sudah menjadi obat untuk semuanya. Karena aku percaya bahwa "Usaha tidak akan mengkhianati hasil".</p>
+                </div>
+                <div className="skill" id="skill">
+                    <div className="basic-skill">
+                        <h1>Basic Skill</h1>
+                        <ol>
+                            <li>C</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                        </ol>
+                    </div>
+                    <div className="framework">
+                        <h1>Framework</h1>
+                        <ul>
+                            <li>Bootstrap</li>
+                        </ul>
+                    </div>
+                </div>
+                <form id="contact">
+                    <h1>Contact Me</h1>
+                    <h3>Nama : <input type="text" placeholder="nama"/></h3>
+                    <h3>Email : <input type="email" placeholder="email"/></h3>
+                    <h3>Pesan</h3>
+                    <textarea style={
+                        {resize: 'none', width: '90%', height: '88px'}
+                    }/><br/>
+                    <input type="submit" value="kirim"/>
+                </form>
             </main>
         )
     }
