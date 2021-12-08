1a505379ee00eee3aae2989c6b85476474e83a59
awaludin-dev <ubedsf@gmail.com>
2021-12-08 09:28:56 +0800

    styling redux

--- src/component/Redux/Pembahasan/Counter.jsx
+++ src/component/Redux/Pembahasan/Counter.jsx
@@ -5,7 +5,7 @@ const Counter = () => {
     let {count} = useSelector(state => state.counter);
     const dispatch = useDispatch();
     return(
-        <div>
+        <div style={{display: 'flex', height: '100vh', width: '100vw', alignItems: 'center', justifyContent: 'center'}}>
             <button onClick={() => dispatch(decrementChecking(1))}>-</button>
             {' '}<span>{count}</span>{' '}
             <button onClick={() => dispatch(increment(1))}>+</button>
